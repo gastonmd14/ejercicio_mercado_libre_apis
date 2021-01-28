9ef1f8e3181716c9ed0b5d47fe534e4ad6248499
Dominguez Gaston <gastonmd14@gmail.com>
2021-01-28 13:46:32 -0300

    se corrige la configuracion del fecth para que muestre los productos por categoria, ejercio 4 del primer pdf de apis

--- public/js/categories.js
+++ public/js/categories.js
@@ -32,22 +32,54 @@ window.addEventListener("load", function () {
     
     let hipervinculo = document.querySelectorAll('.product-detail-info a')
      
-    //   console.log(hipervinculo);
-    //   console.log(hipervinculo.outerText);
-          
-       for(let i = 0; i < hipervinculo.length; i++) {
-                fetch('http://localhost:3000/api/products/categories/' + hipervinculo[i].outerText)
-                .then((response) => {
-                   return response.json()
-                })
-                .then((resultado) => {
+   //   console.log(hipervinculo);
+   //   console.log(hipervinculo.outerText);
+         
+      for(let i = 0; i < hipervinculo.length; i++) {
+         hipervinculo[i].addEventListener('click', function(e) {
+
+            e.preventDefault()
+            
+            fetch("http://localhost:3000/api/products/categories/" + hipervinculo[i].outerText)
+               .then((response) => {
+                  return response.json()
+               })
+               .then((resultado) => {
+                  // console.log(resultado.data);
+
+                  let titulo = document.getElementById('title')
+
+                  let cambio1 = document.querySelector(".products-container");
                   
-                   console.log(resultado.data);
-                
-                
-             })
-             
-       }
+                  
+                  resultado.data.forEach((element) => {
+                     console.log(element);
+
+                  titulo.innerHTML = 
+                  `<h1 class="products-title"> ${element.category.name} </h1>`
+                  
+                  let discount = element.discount > 0 ? `<span> ${element.discount} % OFF</span>` : "";
+
+                  cambio1.innerHTML = 
+                  `<div class="col-12 col-sm-6 col-lg-4">
+                        <section class="product-box">
+                           <a href="/products/detail/${element.id} ">
+                              <figure class="product-box_image">
+                                 <img src="/images/products/${element.image}" alt="${element.name}">
+                              </figure>
+                              <article class="product-box_data">
+                                 <h2>${element.price - (element.price * element.discount) / 100} </h2>
+                                    ${discount}
+                                 <p>${element.name} </p>
+                                 <i class="fas fa-truck"></i>
+                              </article>
+                           </a>
+                        </section>
+                     </div>`;
+                  });
+               })
+         })
+      }
             
       
                
